df99b14835c83d30409481607a3ebc6a87bf9852
ivegor <air@Egors-MacBook-Air.local>
2016-09-09 23:28:20 +0300

    likes

--- base/static/app/news/news.controller.js
+++ base/static/app/news/news.controller.js
@@ -3,18 +3,23 @@
     angular
         .module('app')
         .controller('NewsController', news);
-    news.$inject = ['$scope', '$http', 'NewsLikesService'];
-    function news($scope, $http, NewsLikesService){
+    news.$inject = ['$scope', '$http', 'Likes', '$mdMedia'];
+    function news($scope, $http, Likes, $mdMedia){
         var vm = this;
+        var newsObj = $scope.switch.content;
+        var newsLikes = Likes.news;
+        var cur_likes = newsLikes[newsObj.id];
+
         vm.set_like = set_like;
-        var cur_likes = NewsLikesService.getLikes($scope.switch.content.id);
-        $scope.switch.content.like  = cur_likes;
-        $scope.switch.content.additional.likes = cur_likes || $scope.switch.content.additional.likes;
+        vm.size = $mdMedia;
+
+        newsObj.like  = !!cur_likes;
+        newsObj.additional.likes = cur_likes || newsObj.additional.likes;
 
         function set_like(news) {
             if (!news.like){
                 news.additional.likes +=1;
-                NewsLikesService.setLikes(news.id, news.additional.likes);
+                newsLikes[news.id] = news.additional.likes;
                 $http.post('/api/news_like', {id: news.id})
             }
             news.like = true;
--- base/static/app/staff/staff.controller.js
+++ base/static/app/staff/staff.controller.js
@@ -3,21 +3,35 @@
     angular
         .module('app')
         .controller('staff', staff);
-    staff.$inject = ['$mdDialog', '$mdMedia', '$http'];
-    function staff($mdDialog, $mdMedia, $http){
+    staff.$inject = ['$scope', '$mdDialog', '$mdMedia', '$http', 'Likes'];
+    function staff($scope, $mdDialog, $mdMedia, $http, Likes){
         var vm = this;
+        var staffList = $scope.switch.content;
+        var staffLikes = Likes.staff;
+        var st;
+        
         vm.category = ['администрация', 'педагоги', 'обслуживающий персонал'];
         vm.cur_cat = vm.category[0];
         vm.set_like = set_like;
         vm.show = show;
 
+        for (var i = 0; i < staffList.length; i++){
+            st = staffList[i];
+            if (st.additionalinformation){
+                st.like = !!staffLikes[st.id];
+                st.additionalinformation.likes = staffLikes[st.id] || st.additionalinformation.likes
+            }
+        }
+
         function set_like(staff) {
             if (!staff.like){
-                staff.additionalinformation.likes +=1;
+                staff.additionalinformation.likes += 1;
+                staffLikes[staff.id] = staff.additionalinformation.likes;
                 $http.post('/api/like', {id: staff.id})
             }
             staff.like = true;
         }
+
         function show(ev, st) {
             var useFullScreen = $mdMedia('xs');
             $mdDialog.show({
